530106cf5a471eeaa7a08fe590bef3e40f3bd975
oguz gül <ogzgl95@outlook.com>
2021-08-05 19:26:33 +0300

    Design complated Header

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,34 +5,57 @@ import Section from "./Section";
 function Home() {
   return (
     <Container>
-      <Section 
+      <Section
         title="Model S"
         description="Order Online for Touchless Delivery"
-        backgroundImg ="model-s.jpg"
+        backgroundImg="model-s.jpg"
         leftBtnText="Custom Order"
         rightBtnText="Existing inventory"
       />
-      <Section 
+      <Section
         title="Model Y"
         description="Order Online for Touchless Delivery"
-        backgroundImg ="model-y.jpg"
+        backgroundImg="model-y.jpg"
         leftBtnText="Custom Order"
         rightBtnText="Existing inventory"
       />
-      <Section 
+      <Section
         title="Model 3"
         description="Order Online for Touchless Delivery"
-        backgroundImg ="model-3.jpg"
+        backgroundImg="model-3.jpg"
         leftBtnText="Custom Order"
         rightBtnText="Existing inventory"
       />
-      <Section 
+      <Section
         title="Model X"
         description="Order Online for Touchless Delivery"
-        backgroundImg ="model-x.jpg"
+        backgroundImg="model-x.jpg"
         leftBtnText="Custom Order"
         rightBtnText="Existing inventory"
-      /> 
+      />
+
+      <Section
+        title="Lowest Cost Solar Panels in America"
+        description="Money-back guarantee"
+        backgroundImg="solar-panel.jpg"
+        leftBtnText="Order now"
+        rightBtnText="Learn more"
+      />
+
+      <Section
+        title="Solar for New Roofs"
+        description="Solar Roof Costs Less Than a New Roof Plus Solar Panels "
+        backgroundImg="solar-roof.jpg"
+        leftBtnText="Order now"
+        rightBtnText="Learn more"
+      />
+
+      <Section
+        title="Accessories"
+        description=""
+        backgroundImg="accessories.jpg"
+        leftBtnText="Shop now"
+      />
     </Container>
   );
 }
