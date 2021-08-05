eb12f4efc436ecfcbfc3e966e9e770d92efa8a04
oguz gül <ogzgl95@outlook.com>
2021-08-05 16:44:10 +0300

    Added to component Header and Home

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+function Home() {
+  return <Container>Tesla</Container>;
+}
+
+export default Home;
+
+const Container = styled.div`
+  height: 100vh;
+`;
+
+
